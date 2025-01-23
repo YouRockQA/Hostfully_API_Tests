5ac054c16a43d8b5d5be8855af43ccdc552d0ea9
Yurii Beniashevskyi <beniakrytgg@gmail.com>
2025-01-23 16:18:41 -0500

    Updated conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,10 @@
 import pytest
 from utils.api_client import APIClient
+from utils.data_generators import generate_property_payload
+
+@pytest.fixture
+def create_test_property():
+    payload = generate_property_payload()
+    response = APIClient().post("/properties", payload)
+    assert response.status_code == 201
+    return response.json()["id"]
